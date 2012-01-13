a883d72a8922fa5f272694e4edacca1d81122fc4
Chris Williams <cwilliams@appcelerator.com>
2012-01-13 11:06:23 -0500

    Extract UI strings

--- bundle.rb
+++ bundle.rb
@@ -1,42 +1,33 @@
 require 'ruble.rb'
 
-bundle 'Engine Yard' do |bundle|
-  bundle.authoer = 'Allen Yeung'
-  bundle.copyright = <<END
-Copyright 2010 Aptana Inc. Distributed under the MIT license.
-END
-
-  bundle.description = <<END
-Easy deployment with Engine Yard from within Studio 3
-END
+bundle t(:bundle_name) do |bundle|
+  bundle.author = 'Allen Yeung'
+  bundle.copyright = 'Copyright 2010 Aptana Inc. Distributed under the MIT license.'
+  bundle.description = 'Easy deployment with Engine Yard from within Studio 3' 
+  bundle.repository = "git://github.com/aptana/engineyard.ruble.git"
   
-  #bundle.repository = "git://github.com/aptana/engine-yard.ruble.git"
-  
-
-  bundle.menu "Engine Yard" do |menu|
+  bundle.menu t(:bundle_name) do |menu|
     menu.scope = [ "source.ruby", "project.rails" ]
-    
-    menu.command "Deploy App"
+
+    menu.command t(:deploy_app)
     menu.separator
-    menu.command "Open SSH Session"
-    
-    menu.menu "Deployment" do |deploy|
-      deploy.command "List Environments"
-      deploy.command "Retrieve Logs"
-      deploy.command "Rebuild Environment"
-      deploy.command "Rollback App"
+    menu.command t(:open_ssh_session)
+
+    menu.menu t(:deployment) do |deploy|
+      deploy.command t(:list_environments)
+      deploy.command t(:retrieve_logs)
+      deploy.command t(:rebuild_environment)
+      deploy.command t(:rollback_app)
     end
-    menu.menu "Recipes" do |recipe|
-      recipe.command "Apply Recipes"
-      recipe.command "Upload Recipes"
-      recipe.command "Download Recipe"
+    menu.menu t(:recipes) do |recipe|
+      recipe.command t(:apply_recipes)
+      recipe.command t(:upload_recipes)
+      recipe.command t(:download_recipes)
     end
-    menu.menu "Maintenance" do |web|
-      web.command "Turn Maintenance On"
-      web.command "Turn Maintenance Off"
+    menu.menu t(:maintenance) do |web|
+      web.command t(:turn_maintenance_on)
+      web.command t(:turn_maintenance_off)
     end
-    
   end
-  
 end
 
